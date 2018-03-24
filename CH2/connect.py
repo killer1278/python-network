2027653e1b67e45a5ac2aaeb473d58c733ef12ab
killer1278 <1431660012@qq.com>
2018-03-24 20:02:52 +0800

    Create connect.py

---
+++ CH2/connect.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# Basic Connection Example - Chapter 2 - connect.py
+# 这个例子连接上www.baidu.com上的web服务器，接着打印状态信息，最后终止。
+
+import socket
+
+print("Creating socket...")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("done.\n")
+
+print("Connecting to remote host...")
+s.connect(("www.baidu.com",80))
+print("done.\n")
